7b3d7ef98c46183010fed8969237a10e0d2fb0d7
PedroHFG <pedrohfidg@gmail.com>
2024-12-28 11:40:29 -0300

    Created Address entity

---
+++ Course/Entities/Address.cs
@@ -0,0 +1,18 @@
+﻿namespace Course.Entities
+{
+    internal class Address
+    {
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public Address() 
+        { 
+        }
+
+        public Address(string email, string phone)
+        {
+            Email = email;
+            Phone = phone;
+        }
+    }
+}
