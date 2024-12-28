9e00c3adfb88e7283cc37ace34cb1dce7df6a63a
PedroHFG <pedrohfidg@gmail.com>
2024-12-28 11:42:33 -0300

    Created Employee entity

---
+++ Course/Entities/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace Course.Entities
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee() 
+        { 
+        }
+
+        public Employee(string name, double salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+    }
+}
